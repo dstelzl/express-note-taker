2750da5a4f2e81128e6cf065c2782b29b8a1350a
dds git <deeann@stelzl.us>
2022-09-15 15:40:26 -0400

    edited server file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const apiRoutes = require('./routes/apiroutes.js');
 const htmlRoutes = require('./routes/htmlroutes.js');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
