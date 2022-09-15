9de8041e73b34e563d48e776272eddc3d9fcc4f9
dds git <deeann@stelzl.us>
2022-09-15 15:23:07 -0400

    testing api routes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -2,24 +2,24 @@ const router = require('express').Router()
 const db = require('../db/db.json')
 const fs = require('fs');
 
-router.get("/notes", (req, res)=>{
-    console.log('DeeAnn is awesome')
-    res.json(db)
-})
+// router.get("/notes", (req, res)=>{
+//     console.log('DeeAnn is awesome')
+//     res.json(db)
+// })
 
-router.post("/notes", (req, res)=>{
-    console.log(req.body)
-    db.push(req.body)
-    fs.writeFile("./db/db.json", JSON.stringify(db), err => err)
-    res.json(db)
-})
+// router.post("/notes", (req, res)=>{
+//     console.log(req.body)
+//     db.push(req.body)
+//     fs.writeFile("./db/db.json", JSON.stringify(db), err => err)
+//     res.json(db)
+// })
 
-router.delete("/notes/:id", (req, res)=>{
-    console.log(req.params)
-    db.splice(req.params.id-1, 1)
-    fs.writeFile("./db/db.json", JSON.stringify(db), err => err)
-    res.json(db)
-})
+// router.delete("/notes/:id", (req, res)=>{
+//     console.log(req.params)
+//     db.splice(req.params.id-1, 1)
+//     fs.writeFile("./db/db.json", JSON.stringify(db), err => err)
+//     res.json(db)
+// })
 
 //if length is longer than db then response.json("that is not a valid id")
 
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 app.use(express.static('public'));
